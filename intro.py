a17a5af1b79ea3f9fb81d12b4bd98c4ccb262ce4
kateleung <56771045+kateleung@users.noreply.github.com>
2019-10-19 14:37:46 -0400

    wics workshop

---
+++ intro.py
@@ -0,0 +1,25 @@
+print("hello world")
+print("My name is Katelyn and I am 15 years old.")
+
+name = "Katelyn"
+age = 15
+
+print(name, age)
+print(age)
+
+# conditionals
+if name == "Katelyn":
+    print("Wow, hi", name)
+else:
+    print("what's your name?")
+
+# loops 
+count = 10
+
+while count > 0:
+    print(count)
+    count = count - 1
+print()
+
+for count in range(0):
+    print(count-1)
